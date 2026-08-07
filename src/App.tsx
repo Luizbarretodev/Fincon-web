ab7926c07bae69682fe61a56507f725a3253aa2e
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-06 22:09:34 -0300

    feat: adiciona listagem de contas cadastradas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import { criarConta, listarContas } from './services/contaService';
+import type { Conta } from './types/conta';
+import './App.css';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [contas, setContas] = useState<Conta[]>([]);
+
+  useEffect(() => {
+    carregarContas();
+  }, []);
+
+  async function carregarContas() {
+    try {
+      const dados = await listarContas();
+      setContas(dados);
+    } catch (erro) {
+      console.error(erro);
+    }
+  }
+
+  async function handleCriarConta() {
+    try {
+      await criarConta({ nome });
+      setNome('');
+      await carregarContas();
+    } catch (erro) {
+      console.error(erro);
+    }
+  }
+
   return (
     <div>
       <h1>Fincon</h1>
-      <p>Controle financeiro pessoal</p>
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        placeholder="Nome da conta"
+      />
+      <button onClick={handleCriarConta}>Criar Conta</button>
+
+      <h2>Contas cadastradas</h2>
+      <ul>
+        {contas.map((conta) => (
+          <li key={conta.id}>{conta.nome}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/contaService.ts
+++ src/services/contaService.ts
@@ -2,6 +2,18 @@ import type { Conta, CriarContaRequest } from '../types/conta';
 
 const API_URL = 'https://localhost:7211/api';
 
+export async function listarContas(): Promise<Conta[]> {
+    const response = await fetch(`${API_URL}/Contas`, {
+        method: 'GET'
+    });
+
+    if (!response.ok){
+        throw new Error('Erro ao buscar contas');
+    }
+
+    return response.json();
+}
+
 export async function criarConta(request: CriarContaRequest): Promise<Conta> {
     const response = await fetch(`${API_URL}/Contas`, {
         method: 'POST',
