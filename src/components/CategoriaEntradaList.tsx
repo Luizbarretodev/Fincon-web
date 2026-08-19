cf13fe7605183df05900fbc9813586a7aae56656
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-19 14:45:34 -0300

    feat:adiciona funçoes para atualizar e excluir categoriaEntrada

--- src/components/CategoriaEntradaList.tsx
+++ src/components/CategoriaEntradaList.tsx
@@ -1,14 +1,30 @@
 import type { CategoriaEntrada } from '../types/categoria';
+import { excluirCategoriaEntrada } from '../services/CategoriaEntradaService';
 
 interface CategoriaEntradaListProps {
   categoriasEntrada: CategoriaEntrada[];
+  onCategoriaEntradaAlterada: () => void;
+  onEditar: (categoria: CategoriaEntrada) => void;
 }
 
-function CategoriaEntradaList({ categoriasEntrada }: CategoriaEntradaListProps) {
+function CategoriaEntradaList({ categoriasEntrada, onCategoriaEntradaAlterada, onEditar }: CategoriaEntradaListProps) {
+  async function handleExcluir(id: string) {
+    try {
+      await excluirCategoriaEntrada(id);
+      onCategoriaEntradaAlterada();
+    } catch (erro) {
+      console.error(erro);
+    }
+  }
+
   return (
     <ul>
-      {categoriasEntrada.map((categoriaEntrada) => (
-        <li key={categoriaEntrada.id}>{categoriaEntrada.nome}</li>
+      {categoriasEntrada.map((categoria) => (
+        <li key={categoria.id}>
+          {categoria.nome}
+          <button onClick={() => onEditar(categoria)}>Editar</button>
+          <button onClick={() => handleExcluir(categoria.id)}>Excluir</button>
+        </li>
       ))}
     </ul>
   );
