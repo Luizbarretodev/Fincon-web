3814c6d58333d04dbff6b84c2b7b551101f57f51
Luizbarretodev <gugajoao1020@gmail.com>
2026-08-25 18:26:56 -0300

    feat: implementa estado de edição de entradas

--- src/pages/EntradasPage.tsx
+++ src/pages/EntradasPage.tsx
@@ -1,28 +1,34 @@
 import { useState, useEffect } from 'react';
-import { listarEntradas } from '../services/EntradaService';
+import { listarEntradas } from '../services/entradaService';
 import type { Entrada } from '../types/entrada';
 import EntradaForm from '../components/EntradaForm';
 import EntradaList from '../components/EntradaList';
 
-function EntradaPage() {
+function EntradasPage() {
   const [entradas, setEntradas] = useState<Entrada[]>([]);
+  const [entradaEditando, setEntradaEditando] = useState<Entrada | null>(null);
 
   useEffect(() => {
     carregarEntradas();
-  }, []);
+  },  []);
 
   async function carregarEntradas() {
     const dados = await listarEntradas();
     setEntradas(dados);
   }
 
+  function handleSalvar() {
+    setEntradaEditando(null);
+    carregarEntradas();
+  }
+
   return (
     <div>
       <h2>Entradas</h2>
-      <EntradaForm onEntradaCriada={carregarEntradas} />
-      <EntradaList entradas={entradas} />
+      <EntradaForm entradaEditando={entradaEditando} onSalvar={handleSalvar} />
+      <EntradaList entradas={entradas} onEntradaAlterada={carregarEntradas} onEditar={setEntradaEditando} />
     </div>
   );
 }
 
-export default EntradaPage;
+export default EntradasPage;
